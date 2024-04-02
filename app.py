6210d2f090c6b25a83bc39376fedbf1fcc8dc36f
tarapoco <157441310+tarapoco@users.noreply.github.com>
2024-04-02 11:44:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def video_reply():
     "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
     input={
         "prompt": q,
-        "num_frames": 10,
+        "num_frames": 5,
     },
 )
     time.sleep(60)
